15ce84f541417fbff1b4f9d3cd39d882fb58d8cb
sainani3067 <ammu97938@gmail.com>
2021-09-23 17:45:34 +0530

    Day8assignment

---
+++ Part time Employee wage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise_problems_8
+{
+    class Part_time_Employee_wage
+    {
+        public static void PartTimeEmpWage()
+        {
+            //Constants
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+
+        }
+
+    }
+}
+
